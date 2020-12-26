0b25ff4d4a3953a39a54f411a00bb7dc729e2d1a
samsung <samsung@DESKTOP-BUPJ5J1>
2020-12-26 17:35:54 +0900

    initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,9 @@
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Yak, husky");
+		System.out.println("야 야 야 야 야");
+		System.out.println("짱구 시간표 만들기");
+	}
+}
